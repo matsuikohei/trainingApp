0fc4204085138f44bf727532e28f71d54e3741be
matsuikohei <kmatsui0612@gmail.com>
2022-05-25 19:51:02 +0900

    編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_params, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_params
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :employee_number, :last_name, :first_name)
   end
 end
