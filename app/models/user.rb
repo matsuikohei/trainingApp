26e607a1fdc2e9f911690263cef6dddbf6f68e6f
matsuikohei <kmatsui0612@gmail.com>
2022-05-26 13:22:14 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :employee_number, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  with_options presence: true do
+    validates :employee_number,uniqueness: true, format: { with: /\A[0-9]{10}\z/ }
+    validates :last_name, format: { with: /\A[a-zぁ-んァ-ヶ一-龥々ー]+\z/i}
+    validates :first_name, format: { with: /\A[a-zぁ-んァ-ヶ一-龥々ー]+\z/i}
+  end
 end
