5e5f3d8294ba6422adbf13132e713522ab742b91
matsuikohei <kmatsui0612@gmail.com>
2022-05-27 19:16:44 +0900

    研修内容投稿機能の実装

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -7,5 +7,16 @@ class TrainingsController < ApplicationController
   end
 
   def create
+    @training = Training.new(training_params)
+    if @training.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def training_params
+    params.require(:training).permit(:trainer_id, :training_date, :training_category_id, :content, user_ids: [])
   end
 end
