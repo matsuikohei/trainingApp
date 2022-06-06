add27317126b9f28d4eeaa2ba88bd0e0b6bba56a
matsuikohei <kmatsui0612@gmail.com>
2022-06-06 19:06:29 +0900

    バリデーションの記述

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,11 @@ class Test < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :training_category
+
+  with_options presence: true do
+    validates :test_date
+    validates :test_category_id, numericality: { other_than: 1 }
+    validates :result
+    validates :examiner_id
+  end
 end
