a17caa73d150f571034df3a36d943e2d9bdfa596
matsuikohei <kmatsui0612@gmail.com>
2022-06-06 19:02:24 +0900

    試験結果投稿機能の実装

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,5 +4,16 @@ class TestsController < ApplicationController
   end
 
   def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def test_params
+    params.require(:test).permit(:test_date, :test_category_id, :result)
   end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
-
   
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :training_category
 end
