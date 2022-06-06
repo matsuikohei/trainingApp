71b78a13696169447adfcbc8f5b2bb7f443ba21b
matsuikohei <kmatsui0612@gmail.com>
2022-06-06 19:11:25 +0900

    中間テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_095747) do
+ActiveRecord::Schema.define(version: 2022_06_06_100731) do
+
+  create_table "test_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_test_users_on_test_id"
+    t.index ["user_id"], name: "index_test_users_on_user_id"
+  end
 
   create_table "tests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "test_date", null: false
@@ -65,6 +74,8 @@ ActiveRecord::Schema.define(version: 2022_06_06_095747) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "test_users", "tests"
+  add_foreign_key "test_users", "users"
   add_foreign_key "training_comments", "trainings"
   add_foreign_key "training_comments", "users"
   add_foreign_key "training_users", "trainings"
