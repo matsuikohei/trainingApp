642aa4f99cebb9a0ae8afa4e892cfd3f33cdb311
matsuikohei <kmatsui0612@gmail.com>
2022-06-07 15:37:01 +0900

    test_commentsテーブルの作成

---
+++ app/models/test_comment.rb
@@ -0,0 +1,2 @@
+class TestComment < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_103019) do
+ActiveRecord::Schema.define(version: 2022_06_07_063509) do
+
+  create_table "test_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "test_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_test_comments_on_test_id"
+    t.index ["user_id"], name: "index_test_comments_on_user_id"
+  end
 
   create_table "test_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -75,6 +85,8 @@ ActiveRecord::Schema.define(version: 2022_06_06_103019) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "test_comments", "tests"
+  add_foreign_key "test_comments", "users"
   add_foreign_key "test_users", "tests"
   add_foreign_key "test_users", "users"
   add_foreign_key "training_comments", "trainings"
